3b338f94fe8a889228998bb64d123accd0a37899
Gpraveen <gpraveen.reddy@gmail.com>
2013-12-23 19:59:09 -0800

    typo:

--- aws-php/recipes/php.rb
+++ aws-php/recipes/php.rb
@@ -30,7 +30,7 @@ if platform?("ubuntu")
     uri "http://ppa.launchpad.net/ondrej/php5/ubuntu"
     distribution node["lsb"]["codename"]
     components ["main"]
-    keyserver node["php5_ppa"]["keyserver"]
+    keyserver "keyserver.ubuntu.com"
     key "E5267A6C"
     action :add
     notifies :run, "execute[apt-get update]", :immediately
